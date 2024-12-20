b9d9e30416c15e990347305a08e2e61e8f9449c7
Micha <m.hobert@shopware.com>
2024-12-20 14:39:32 +0100

    Init commit

---
+++ src/AcademyTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Academy;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class AcademyTheme extends Plugin implements ThemeInterface
+{
+}
